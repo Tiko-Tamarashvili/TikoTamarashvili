1c1f3df776061db58994e2beea66d9a2ee071a35
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-10-23 21:20:08 +0400

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.X86;
+
+Random random = new Random();
+int randomNumber = random.Next(1, 10);
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+if (a == randomNumber)
+{
+    Console.WriteLine("you won");
+}
+else Console.WriteLine("Try again");
+a = Convert.ToInt32(Console.ReadLine());
+if (a == randomNumber)
+{
+    Console.WriteLine("you won");
+}
+else Console.WriteLine("Try again");
+a = Convert.ToInt32(Console.ReadLine());
+if (a == randomNumber)
+{
+    Console.WriteLine("you won");
+}
+
+else Console.WriteLine("you lost, correct number is " + randomNumber);
+
