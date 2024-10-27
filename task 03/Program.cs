8440e806d890fabf86e0d8a299839b83bffc7ea4
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-10-27 23:36:10 +0400

    task03

---
+++ task 03/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//davaleba 1
+
+//int a = int.Parse(Console.ReadLine());
+//switch (a)
+//{
+
+//    case 1:
+//        Console.WriteLine("kvira");
+//        break;
+//    case 2:
+//        Console.WriteLine("orshabati");
+//        break;
+//    case 3:
+//        Console.WriteLine("samshabati");
+//        break;
+//    case 4:
+//        Console.WriteLine("otxshabati");
+//        break;
+//    case 5:
+//        Console.WriteLine("xutshabati");
+//        break;
+//    case 6:
+//        Console.WriteLine("paraskevi");
+//        break;
+//    case 7:
+//        Console.WriteLine("shabati");
+//        break;
+//    default: Console.WriteLine("araswori mnishvneloba");
+//        break;
+//}
+
+//davaleba 2 
+
+
+
+//int b = 0;
+//int c;
+//while (true)
+//{
+//    c = int.Parse(Console.ReadLine());
+//    if (c < 0)
+//        break;
+//    b = b + c;
+//}
+//Console.WriteLine(b);
+
+
+
+//davaleba 3
+
+
+int d = int.Parse(Console.ReadLine());
+for (int i = 0; i <= 10; i++) 
+{
+    Console.WriteLine(d +"*"+i+"=" + d * i);
+}
