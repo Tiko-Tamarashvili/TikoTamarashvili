8542cdd0f2689428c1fd1c38b64026316412acbb
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-10-30 00:15:20 +0400

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] students = new string[3];
+for (int i = 0; i < students.Length; i++) 
+    {
+        students[i] = Console.ReadLine();
+    }
+
+for (int i = 0; i < students.Length; i++)
+    {
+        Console.Write(students[i] + " ");
+    }
+
+Console.WriteLine();
+
+
+int[,] grades = new int[3, 2];
+for (int i = 0; i < grades.GetLength(0); i++)
+    {
+        Console.WriteLine("shemoitanet " + students[i] + "s qulebi");
+        for (int j = 0; j < grades.GetLength(1); j++)
+            {
+                grades [i,j] = int.Parse(Console.ReadLine());
+            }
+    }
+
+
+for (int i = 0; i < students.Length; i++)
+    {
+        Console.Write(students[i] + " ");
+        for (int j = 0; j < grades.GetLength(1); j++)
+            {
+            Console.Write(grades[i, j] + " ");
+            }   
+        Console.WriteLine();
+    }
+
+
+
