218701e0bcfe6df4cc4acc11f87a789df1de4fec
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-11-06 01:07:53 +0400

    task07

---
+++ task 07/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using books;
+
+namespace Libraries
+{
+    internal class Library
+    {
+        private Book[] books = new Book[20];
+        private int BookSize = 0;
+        public void AddBook(string Title, string Author, int Year) 
+            {
+                books[BookSize] = new Book(Title, Author, Year);
+                BookSize = BookSize + 1;
+
+            }
+        public void PrintBook()
+            {
+                for (int i = 0; i < BookSize; i++)
+                {
+                    Console.WriteLine(books[i].Title + " " + books[i].Author);
+                }
+            }
+
+        public int Count() 
+            {
+                return BookSize;
+            }
+
+        public void RemoveBook()
+            {
+                books = [];
+                BookSize = 0;
+            }
+
+        public void FindBook(string Title)
+            {
+                for (int i = 0; i < BookSize; i++)
+                {
+                    if(books[i].Title == Title)
+                    {
+                        Console.WriteLine(books[i].Title + " " + books[i].Author);
+                    }
+                }
+            }
+    }
+
+}
---
+++ task 07/Program.cs
@@ -0,0 +1,13 @@
+﻿using Libraries;
+
+Library library = new Library();
+library.AddBook("wigni pirveli", "aaa", 1111);
+library.AddBook("wigni meore", "bbb", 2222);
+library.AddBook("wigni mesame", "ccc", 3333);
+library.PrintBook();
+
+Console.WriteLine(library.Count());
+
+library.FindBook("wigni meore");
+library.RemoveBook();
+Console.WriteLine(library.Count());
