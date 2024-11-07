09faf5b73bea60fd9683b97ea6589d9411df33ed
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-11-08 00:13:41 +0400

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using person;
+
+
+int Age;
+string Name;
+
+Age = 35;
+Name = "tiko";
+
+Person person = new Person(Age, Name);
+
+Person person2 = person;
+person2.Age = 37;
+person2.Name = "niko";
+
+Console.WriteLine(person2.Age);
+Console.WriteLine(person2.Name);
