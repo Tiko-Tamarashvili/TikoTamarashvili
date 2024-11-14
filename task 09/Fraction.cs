da08518ee44e34f3c87af8b17cb54fed51db4c81
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-11-14 23:56:56 +0400

    task09

---
+++ task 09/Fraction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions
+{
+    public class Fraction
+    {
+        public Fraction(int numerator, int denumerator)
+        {
+            if (denumerator == 0)
+            {
+                throw new ArgumentException("0 ze gayofa sheudzlebelia");
+            }
+                
+            _numerator = numerator;
+            _denumerator = denumerator;
+
+        }
+
+        private int _numerator  { get; set; }
+        private int _denumerator { get; set; }
+
+
+        public override string ToString()
+        {
+            return _numerator + "/" + _denumerator;
+        }
+
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            int commonDenumerator = a._denumerator * b._denumerator;
+            int newNumerator = a._numerator * b._denumerator + b._numerator * a._denumerator;
+            return new Fraction(newNumerator, commonDenumerator);
+        }
+
+
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            
+            return a._numerator == b._numerator && a._denumerator == b._denumerator;
+        }
+
+
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+
+            return a._numerator != b._numerator  || a._denumerator != b._denumerator;
+        }
+    }
+
+   
+}
---
+++ task 09/Program.cs
@@ -0,0 +1,11 @@
+﻿using Fractions;
+
+Fraction fraction = new Fraction(4, 8);
+
+Console.WriteLine (fraction.ToString());
+
+Fraction fraction1 = new Fraction(3, 9);
+Fraction fraction2 = new Fraction(5, 6);
+
+Console.WriteLine((fraction1 + fraction2).ToString());
+Console.WriteLine(fraction1 == fraction2);
