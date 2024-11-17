0356cea188130b92abb7ae2c9a30b2f9448f354d
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-11-18 01:35:20 +0400

    task11.2

---
+++ task11.2/MathHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathhelper;
+
+static class MathHelper
+{
+    public static int Add (int a, int b)
+    { 
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply (int a, int b) 
+    {
+        return a * b;
+    }
+
+    public static int Divide(int a, int b)
+
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("nulze gayofa daushvebelia");
+        }
+        return a / b; 
+    }
+
+
+}
---
+++ task11.2/Program.cs
@@ -0,0 +1,6 @@
+﻿using mathhelper;
+
+Console.WriteLine (MathHelper.Add(13, 15));
+Console.WriteLine(MathHelper.Subtract(25, 10));
+Console.WriteLine(MathHelper.Multiply(10, 10));
+Console.WriteLine(MathHelper.Divide(27, 3));
