e54a97354a50f447789505a0940ac3d62533720a
Tiko-Tamarashvili <tamarashvili.tiko@gmail.com>
2024-11-18 01:20:03 +0400

    task11

---
+++ task11/Counter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter1;
+
+    internal class Counter
+    {
+        static int _count = 0;
+
+
+        public Counter()
+        {
+            _count++;
+        }
+
+
+        public static int GetCount()
+        {
+            return _count;
+        }
+
+        public static void ResetCount()
+        {
+            _count = 0;
+        }
+
+}
+
+
+   
+
---
+++ task11/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Counter1;
+
+ 
+    Counter counter = new Counter();
+    Console.WriteLine(Counter.GetCount());
+
+    Counter counter1 = new Counter();
+    Console.WriteLine(Counter.GetCount());
+
+    Counter counter2 = new Counter();
+    Console.WriteLine(Counter.GetCount());
+
+    Counter counter3 = new Counter();
+    Console.WriteLine(Counter.GetCount());
+
+
+    Counter.ResetCount();
+    Console.WriteLine(Counter.GetCount());
